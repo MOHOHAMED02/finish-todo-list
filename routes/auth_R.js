a58edebaff6526aca8362402cb61f8635096508a
mohamedshibli <mohamed2002ahibli@gmail.com>
2026-01-28 22:23:02 +0200

    auth_R.js

--- routes/auth_R.js
+++ routes/auth_R.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const router = express.Router();
 
 const {valuesToAdd,encrypPass,valuesToLogin} = require('../middelware/auth_MID.js');
