80ae3c8ea8546618de00eb3a640b8a85ad9d2460
mohamedshibli <mohamed2002ahibli@gmail.com>
2026-01-28 22:21:07 +0200

    Fedet SQL query formatting in getAll function

--- model/tasks_M.js
+++ model/tasks_M.js
@@ -1,7 +1,7 @@
 const db = require('../config/db_config');
 
 async function getAll(userId){
-    let sql = `SELECT * FROM tasks WHERE user_id = ?`;
+    let sql = `SELECT * FROM tasks WHERE user_id = ?`; 
     let [rows] = await db.query(sql,[userId]);    
     return rows;
 }
