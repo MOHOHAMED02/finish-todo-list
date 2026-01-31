c6f5321b308c08a2f0a4cd31007eab51434e5166
mohamedshibli <mohamed2002ahibli@gmail.com>
2026-01-31 03:16:21 +0200

    fnishauth_R.js

--- routes/auth_R.js
+++ routes/auth_R.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-
 const auth_MID = require('../middelware/auth_MID');
 
-
 const auth_C = require('../controller/auth_C');
-
-
 
 router.post('/register', auth_MID.valuesToAdd, auth_MID.encrypPass, auth_C.register);
 
